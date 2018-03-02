0f51ea554e76b947009bcb5968bbb85c014bc44a
Dmitriy Virych <dmitry.virych@requestum.com>
2018-03-02 14:19:17 +0200

    added main controller class

---
+++ SecurityManager/Classes/Controllers/MainController.swift
@@ -0,0 +1,13 @@
+//
+//  MainController.swift
+//  SecurityManager
+//
+//  Created by Dmitriy Virych on 3/2/18.
+//  Copyright © 2018 Requestum. All rights reserved.
+//
+
+import Foundation
+
+class MainController {
+    
+}
