8df0cd214f959a63bcef35a813215696f13bf8c6
Dmitriy Virych <dmitry.virych@requestum.com>
2018-03-02 13:40:11 +0200

    Initial Commit

---
+++ SecurityManager/SecurityManager.h
@@ -0,0 +1,19 @@
+//
+//  SecurityManager.h
+//  SecurityManager
+//
+//  Created by Dmitriy Virych on 3/2/18.
+//  Copyright © 2018 Requestum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecurityManager.
+FOUNDATION_EXPORT double SecurityManagerVersionNumber;
+
+//! Project version string for SecurityManager.
+FOUNDATION_EXPORT const unsigned char SecurityManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecurityManager/PublicHeader.h>
+
+
